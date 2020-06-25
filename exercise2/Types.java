d7eeb9cf8658fd8d11981bd21a1893ff7a04e5f3
John Kol <Kolja0@protonmail.com>
2020-06-25 04:00:42 -0500

    Added types.java

--- exercise2/Types.java
+++ exercise2/Types.java
@@ -1,5 +1,7 @@
 package exercise2;
 
+import java.util.Arrays;
+
 class Types {
 //    This class has syntax errors, that will prevent your other classes from running.  So... work on this file first to resolve the issues.
 //    boolean number;
@@ -14,4 +16,18 @@ class Types {
 //    public static void main() {
 //        System.out.println("There are variables that have been declared in this Accessible class.  Fix the code so that all the variables can be accessed by the main method.  print out the values of every variable once the code has been refactored.");
 //    }
+
+    public static void main(String[] args) {
+        boolean number = false;
+        int[] myArray = {3,4,5,6,76,12,2};
+        String movieA = "Harry and the Hendersons";
+        String movieB = "Star Wars";
+        char male = 'm';
+        char female = 'f';
+
+        System.out.println(number);
+        System.out.println(Arrays.toString(myArray));
+        System.out.println(movieA + " - " + movieB);
+        System.out.println(male + " - " + female);
+    }
 }
